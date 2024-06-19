27026e42f2e2f921900772b660260beb23514aa9
wilbur.bs <wilbur.bsg@gmail.com>
2024-06-19 14:38:04 -0400

    gameworld trigger

--- GameWorld/Customer/Customer.cs
+++ GameWorld/Customer/Customer.cs
@@ -1,20 +1,22 @@
 using Godot;
 using System;
 
-public partial class GameWorldController : Node3D
+public partial class Customer : Node3D
 {
 	[Export]
-	public Area3D area;
+	public Vector3 MovementVector = new Vector3(1,0,0);
+	[Export]
+	public float Speed = 1;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		area.AreaEntered += (enterer) => GD.Print("Food Trunk entered by " + enterer.GetParent().Name);
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Position = Position + (MovementVector * Speed * (float)delta);
 	}
-
-
 }
